a3bf528b9624612347d9498abde77731f6bec1f4
Zacharsky <mateusz.zacharski1@gmail.com>
2018-10-27 15:59:36 +0200

    First

---
+++ articalApp/src/main/java/pl/kodu/akademia/articalApp/ArticalAppApplication.java
@@ -0,0 +1,12 @@
+package pl.kodu.akademia.articalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticalAppApplication.class, args);
+	}
+}
